79012172959a7db977b74db6d978a1901fdad815
Viktor Ptitselov <qbit86@gmail.com>
2024-11-20 22:24:36 +0200

    Add PartOnePuzzle_Tests and PartTwoPuzzle_Tests

---
+++ AdventOfCode2024-CSharp/tests/Tests.Puzzles/PartOnePuzzle_Tests.cs
@@ -0,0 +1,12 @@
+﻿namespace AdventOfCode2024;
+
+public sealed class PartOnePuzzle_Tests
+{
+    [Theory]
+    [InlineData("sample.txt", long.MinValue)]
+    internal void Solve(string inputPath, long expected)
+    {
+        long actual = PartOnePuzzle.Solve(inputPath);
+        Assert.Equal(expected, actual);
+    }
+}
---
+++ AdventOfCode2024-CSharp/tests/Tests.Puzzles/PartTwoPuzzle_Tests.cs
@@ -0,0 +1,14 @@
+#if false
+namespace AdventOfCode2024;
+
+public sealed class PartTwoPuzzle_Tests
+{
+    [Theory]
+    [InlineData("sample.txt", long.MinValue)]
+    internal void Solve(string inputPath, long expected)
+    {
+        long actual = PartOnePuzzle.Solve(inputPath);
+        Assert.Equal(expected, actual);
+    }
+}
+#endif
