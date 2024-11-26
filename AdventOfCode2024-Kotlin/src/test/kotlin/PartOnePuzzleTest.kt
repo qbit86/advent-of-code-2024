03f5af33c76b6e0f682f93cda39b932222c49d6e
Viktor Ptitselov <qbit86@gmail.com>
2024-11-26 23:59:44 +0200

    Add simple tests

---
+++ AdventOfCode2024-Kotlin/src/test/kotlin/PartOnePuzzleTest.kt
@@ -0,0 +1,12 @@
+import com.adventofcode2024.PartOnePuzzle
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class PartOnePuzzleTest {
+
+    @Test
+    fun solve() {
+        val expected = 0L
+        assertEquals(expected, PartOnePuzzle.solve("../assets/sample.txt"))
+    }
+}
---
+++ AdventOfCode2024-Kotlin/src/test/kotlin/PartTwoPuzzleTest.kt
@@ -0,0 +1,12 @@
+import com.adventofcode2024.PartTwoPuzzle
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class PartTwoPuzzleTest {
+
+    @Test
+    fun solve() {
+        val expected = 0L
+        assertEquals(expected, PartTwoPuzzle.solve("../assets/sample.txt"))
+    }
+}
