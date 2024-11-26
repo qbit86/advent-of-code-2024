5d896c24ef09460733a954f54886b6da76333fd0
Viktor Ptitselov <qbit86@gmail.com>
2024-11-26 02:53:12 +0200

    Add project AdventOfCode2024.App

---
+++ AdventOfCode2024-CSharp/src/AdventOfCode2024.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdventOfCode2024;
+
+internal static class Program
+{
+    private static void Main()
+    {
+        long answer = PartOnePuzzle.Solve("input.txt");
+        Console.WriteLine(answer);
+    }
+}
