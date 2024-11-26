528601233b46ea759c07d029c4d976e7c705078d
Viktor Ptitselov <qbit86@gmail.com>
2024-11-27 00:33:55 +0200

    Add `@ParameterizedTest`

--- AdventOfCode2024-Kotlin/src/test/kotlin/PartOnePuzzleTest.kt
+++ AdventOfCode2024-Kotlin/src/test/kotlin/PartOnePuzzleTest.kt
@@ -1,12 +1,24 @@
 import com.adventofcode2024.PartOnePuzzle
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
 
 class PartOnePuzzleTest {
 
-    @Test
-    fun solve() {
-        val expected = 0L
-        assertEquals(expected, PartOnePuzzle.solve("../assets/sample.txt"))
+    @ParameterizedTest
+    @MethodSource("inputProvider")
+    fun solve(inputPath: String, expected: Long) {
+        assertEquals(expected, PartOnePuzzle.solve(inputPath))
+    }
+
+    companion object {
+        @JvmStatic
+        fun inputProvider(): Stream<Arguments> {
+            return Stream.of(
+                Arguments.of("../assets/sample.txt", Long.MIN_VALUE)
+            )
+        }
     }
 }
