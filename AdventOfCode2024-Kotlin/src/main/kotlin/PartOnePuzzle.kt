a036345268a65f9f805f3be23ce29f9b904f02ec
Viktor Ptitselov <qbit86@gmail.com>
2024-12-01 18:07:27 +0200

    [Kotlin] Refactor boilerplate

--- AdventOfCode2024-Kotlin/src/main/kotlin/PartOnePuzzle.kt
+++ AdventOfCode2024-Kotlin/src/main/kotlin/PartOnePuzzle.kt
@@ -1,12 +1,11 @@
 package com.adventofcode2024
 
 import java.io.File
-import java.nio.charset.StandardCharsets
 
 object PartOnePuzzle {
     fun solve(path: String): Long {
         require(path.isNotEmpty()) { "Path cannot be null or empty" }
-        val lines = File(path).readLines(StandardCharsets.UTF_8)
+        val lines = File(path).readLines()
         return solve(lines)
     }
 
