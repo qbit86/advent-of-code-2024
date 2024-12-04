c2ef765018fe991ecff6bbca18c2990b1a8f1020
Viktor Ptitselov <qbit86@gmail.com>
2024-12-05 01:25:11 +0200

    Improve `Main()` so switch to Part Two is no longer required

--- AdventOfCode2024-CSharp/src/AdventOfCode2024.App/Program.cs
+++ AdventOfCode2024-CSharp/src/AdventOfCode2024.App/Program.cs
@@ -6,7 +6,20 @@ internal static class Program
 {
     private static void Main()
     {
-        long answer = PartOnePuzzle.Solve("input.txt");
+        long answer = SolveSingle();
         Console.WriteLine(answer);
+    }
+
+    private static long SolveSingle()
+    {
+        const string path = "input.txt";
+        try
+        {
+            return PartTwoPuzzle.Solve(path);
+        }
+        catch (NotImplementedException)
+        {
+            return PartOnePuzzle.Solve(path);
+        }
     }
 }
--- AdventOfCode2024-Kotlin/src/main/kotlin/Main.kt
+++ AdventOfCode2024-Kotlin/src/main/kotlin/Main.kt
@@ -1,6 +1,15 @@
 package com.adventofcode2024
 
 fun main() {
-    val answer: Long = PartOnePuzzle.solve("../assets/input.txt")
+    val answer: Long = solveSingle()
     println(answer)
+}
+
+private fun solveSingle(): Long {
+    val path = "../assets/input.txt"
+    return try {
+        PartTwoPuzzle.solve(path)
+    } catch (e: NotImplementedError) {
+        PartOnePuzzle.solve(path)
+    }
 }
