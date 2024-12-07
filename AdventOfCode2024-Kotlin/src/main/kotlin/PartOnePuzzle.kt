6a2c2374bbc58078f4966b27b44b3cdf145a3b86
Viktor Ptitselov <qbit86@gmail.com>
2024-12-08 01:31:01 +0200

    [Kotlin] Add solution for "Day 4: Ceres Search"

--- AdventOfCode2024-Kotlin/src/main/kotlin/PartOnePuzzle.kt
+++ AdventOfCode2024-Kotlin/src/main/kotlin/PartOnePuzzle.kt
@@ -3,11 +3,81 @@ package com.adventofcode2024
 import java.io.File
 
 object PartOnePuzzle {
+    private const val PATTERN = "XMAS"
+    private const val REVERSED_PATTERN = "SAMX"
+    private const val PATTERN_LENGTH = 4
+
     fun solve(path: String): Long {
-        require(path.isNotEmpty()) { "Path cannot be null or empty" }
+        require(path.isNotEmpty()) { "Path cannot be null or empty." }
         val lines = File(path).readLines()
         return solve(lines)
     }
 
-    private fun <TRows : List<String>> solve(rows: TRows): Long = TODO()
+    private fun <TRows> solve(rows: TRows): Long where TRows : List<String> {
+        var horizontalCount = 0L
+        var verticalCount = 0L
+        var diagonalCount = 0L
+        var antiDiagonalCount = 0L
+
+        for (i in rows.indices) {
+            for (j in rows[i].indices) {
+                horizontalCount += if (hasHorizontal(rows, i, j)) 1 else 0
+                verticalCount += if (hasVertical(rows, i, j)) 1 else 0
+                diagonalCount += if (hasDiagonal(rows, i, j)) 1 else 0
+                antiDiagonalCount += if (hasAntiDiagonal(rows, i, j)) 1 else 0
+            }
+        }
+
+        return horizontalCount + verticalCount + diagonalCount + antiDiagonalCount
+    }
+
+    private fun <TRows> hasHorizontal(
+        rows: TRows,
+        rowIndex: Int,
+        columnIndex: Int
+    ): Boolean where TRows : List<String> {
+        val row = rows[rowIndex]
+        if (columnIndex + PATTERN_LENGTH > row.length) return false
+        val span = row.substring(columnIndex, columnIndex + PATTERN_LENGTH)
+        return span == PATTERN || span == REVERSED_PATTERN
+    }
+
+    private fun <TRows> hasVertical(rows: TRows, rowIndex: Int, columnIndex: Int): Boolean where TRows : List<String> {
+        if (rowIndex + PATTERN_LENGTH > rows.size) return false
+        val span = CharArray(PATTERN_LENGTH)
+
+        for (i in 0 until PATTERN_LENGTH) {
+            span[i] = rows[rowIndex + i][columnIndex]
+        }
+
+        return String(span) == PATTERN || String(span) == REVERSED_PATTERN
+    }
+
+    private fun <TRows> hasDiagonal(rows: TRows, rowIndex: Int, columnIndex: Int): Boolean where TRows : List<String> {
+        if (columnIndex + PATTERN_LENGTH > rows[rowIndex].length || rowIndex + PATTERN_LENGTH > rows.size) return false
+
+        val span = CharArray(PATTERN_LENGTH)
+
+        for (i in 0 until PATTERN_LENGTH) {
+            span[i] = rows[rowIndex + i][columnIndex + i]
+        }
+
+        return String(span) == PATTERN || String(span) == REVERSED_PATTERN
+    }
+
+    private fun <TRows> hasAntiDiagonal(
+        rows: TRows,
+        rowIndex: Int,
+        columnIndex: Int
+    ): Boolean where TRows : List<String> {
+        if (columnIndex + PATTERN_LENGTH > rows[rowIndex].length || rowIndex + PATTERN_LENGTH > rows.size) return false
+
+        val span = CharArray(PATTERN_LENGTH)
+
+        for (i in 0 until PATTERN_LENGTH) {
+            span[i] = rows[rowIndex + PATTERN_LENGTH - 1 - i][columnIndex + i]
+        }
+
+        return String(span) == PATTERN || String(span) == REVERSED_PATTERN
+    }
 }
--- AdventOfCode2024-Kotlin/src/main/kotlin/PartTwoPuzzle.kt
+++ AdventOfCode2024-Kotlin/src/main/kotlin/PartTwoPuzzle.kt
@@ -3,11 +3,46 @@ package com.adventofcode2024
 import java.io.File
 
 object PartTwoPuzzle {
+    private const val PATTERN = "MS"
+    private const val REVERSED_PATTERN = "SM"
+    private const val WINDOW_SIZE = 3
+
     fun solve(path: String): Long {
-        require(path.isNotEmpty()) { "Path cannot be null or empty" }
+        require(path.isNotEmpty()) { "Path cannot be null or empty." }
         val lines = File(path).readLines()
         return solve(lines)
     }
 
-    private fun <TRows : List<String>> solve(rows: TRows): Long = TODO()
+    private fun <TRows> solve(rows: TRows): Long where TRows : List<String> {
+        var count = 0
+        val horizontalBound = rows.size - WINDOW_SIZE + 1
+
+        for (i in 0 until horizontalBound) {
+            val verticalBound = rows[i].length - WINDOW_SIZE + 1
+            for (j in 0 until verticalBound) {
+                count += if (containsPattern(rows, i, j)) 1 else 0
+            }
+        }
+
+        return count.toLong()
+    }
+
+    private fun <TRows> containsPattern(
+        rows: TRows,
+        rowIndex: Int,
+        columnIndex: Int
+    ): Boolean where TRows : List<String> {
+        if (rows[rowIndex + 1][columnIndex + 1] != 'A') return false
+
+        val span = CharArray(PATTERN.length)
+        span[0] = rows[rowIndex][columnIndex]
+        span[1] = rows[rowIndex + 2][columnIndex + 2]
+
+        if (String(span) != PATTERN && String(span) != REVERSED_PATTERN) return false
+
+        span[0] = rows[rowIndex + 2][columnIndex]
+        span[1] = rows[rowIndex][columnIndex + 2]
+
+        return String(span) == PATTERN || String(span) == REVERSED_PATTERN
+    }
 }
